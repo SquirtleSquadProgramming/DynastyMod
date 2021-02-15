9e315daeeeef2144f144195ea7ae9c3e2f29f78a
kylerosser <kylerossernz@gmail.com>
2021-02-15 22:54:10 +1300

    Added base item and functionality
    DynastyPlayer.cs:
    Added papercrane accessory variables
    Added Hurt event functionality
    
    PaperCrane.cs:
    Added base item and functionality
    
    PaperCrane.png:
    Added placeholder art

---
+++ DynastyPlayer.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameInput;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+
+namespace DynastyMod
+{
+    // ModPlayer classes provide a way to attach data to Players and act on that data.
+    public class DynastyPlayer : ModPlayer
+    {
+        public bool paperCrane;
+
+        public override void ResetEffects()
+        {
+            paperCrane = false;
+        }
+
+        public override void Hurt(bool pvp, bool quiet, double damage, int hitDirection, bool crit)
+        {
+            // If the player is moving faster than a velocity of 3 and they have a paper crane equipped, award silver coins on damage
+            if ((player.bodyVelocity.X > 3 || player.bodyVelocity.Y > 3 || player.bodyVelocity.X < 3 || player.bodyVelocity.Y < 3) && paperCrane)
+            {
+                player.QuickSpawnItem(ItemID.SilverCoin, Convert.ToInt32(damage / 12));
+            }
+        }
+    }
+}
---
+++ Items/Accessories/PaperCrane.cs
@@ -0,0 +1,38 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Utilities;
+
+namespace DynastyMod.Items.Accessories
+{
+    public class PaperCrane : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Heals you by 20 health for every 200 mana consumed.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 20;
+            item.accessory = true;
+            item.value = Item.sellPrice(silver: 30);
+            item.rare = ItemRarityID.Blue;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<DynastyPlayer>().paperCrane = true; 
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(null, "RedEnvelope", 1);
+            recipe.AddIngredient(ItemID.Book, 1);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
