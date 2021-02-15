28265cf065654f7f2105c6c5ad8f790594568cfa
BeefStew <34359784+BeefStew34@users.noreply.github.com>
2021-02-15 23:27:16 +1300

    Update grandma.cs

--- NPCs/grandma.cs
+++ NPCs/grandma.cs
@@ -32,7 +32,7 @@ namespace DynastyMod.NPCs
 		}
 		public override float SpawnChance(NPCSpawnInfo spawnInfo)
 		{
-			return SpawnCondition.OverworldNightMonster.Chance * 0.1f;
+			return SpawnCondition.OverworldNightMonster.Chance * 0.05f;
 		}
 		public override void NPCLoot()
 		{
