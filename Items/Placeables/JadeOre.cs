413c9ae909d854716d0ff070cdf0352c74ca176c
Derek Chai <chderek9@gmail.com>
2021-02-16 14:23:45 +1300

    Added jade ore tilesheet and item

---
+++ Items/Placeables/JadeOre.cs
@@ -0,0 +1,33 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Utilities;
+
+namespace DynastyMod.Items.Accessories
+{
+    public class JadeOre : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("A precious stone praised for its durability and supposed magical qualities.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 16;
+            item.height = 16;
+            item.value = Item.sellPrice(silver: 1);
+            item.rare = ItemRarityID.Green; // temp
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.useTurn = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.autoReuse = true;
+            item.maxStack = 999;
+            item.consumable = true;
+            item.createTile = ModContent.TileType<Tiles.JadeOre>();
+        }
+
+        
+    }
+}
