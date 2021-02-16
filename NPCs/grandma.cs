5b9e078c7de016d205c08a1726316c8ae0aaa8fa
BeefStew <34359784+BeefStew34@users.noreply.github.com>
2021-02-16 15:38:03 +1300

    Update grandma.cs
    Grandma walk on her own

--- NPCs/grandma.cs
+++ NPCs/grandma.cs
@@ -2,6 +2,8 @@
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.Localization;
+using Microsoft.Xna.Framework;
+using System;
 
 namespace DynastyMod.NPCs
 {
@@ -12,9 +14,11 @@ namespace DynastyMod.NPCs
         {
             DisplayName.SetDefault("Grandma");
             Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.Zombie];
-        }
+		}
+		
 		public override void SetDefaults()
 		{
+			
 			npc.width = 18;
 			npc.height = 40;
 			npc.damage = 0;
@@ -24,11 +28,11 @@ namespace DynastyMod.NPCs
 			npc.DeathSound = mod.GetLegacySoundSlot(SoundType.Custom, "Sounds/grandma_death"); ;
 			npc.value = 60f;
 			npc.knockBackResist = 0.5f;
-			npc.aiStyle = 3;
-			aiType = NPCID.Zombie;
+			npc.aiStyle = -1;
+			//aiType = NPCID.Zombie;
 			animationType = NPCID.Zombie;
-			banner = Item.NPCtoBanner(NPCID.Zombie);
-			bannerItem = Item.BannerToItem(banner);
+			//banner = Item.NPCtoBanner(NPCID.Zombie);
+			//bannerItem = Item.BannerToItem(banner);
 		}
 		//Grandma prob wont spawn random might be event npc
 		/*public override float SpawnChance(NPCSpawnInfo spawnInfo)
@@ -38,6 +42,26 @@ namespace DynastyMod.NPCs
 		public override void NPCLoot()
 		{
 			Item.NewItem(npc.getRect(), (short)ModContent.ItemType<Items.RedEnvelope>());
+		}
+		public override void OnHitByProjectile(Projectile projectile,int damage,float knockback,bool crit)
+		{
+			/*if (targetPosition.X < npc.position.X && npc.velocity.X > -8) 
+			{ 
+				npc.velocity.X += 0.22f; // accelerate to the left
+			}
+
+			if (targetPosition.X > npc.position.X && npc.velocity.X < 8)
+			{
+				//npc.spriteDirection = 1;
+				npc.velocity.X -= 0.22f; // accelerate to the right
+			}*/
+		}
+
+		public override void AI()
+		{
+			npc.TargetClosest(false);		
+			npc.spriteDirection = 1;
+			npc.velocity.X = 0.7f;		
 		}
 	}
 }
