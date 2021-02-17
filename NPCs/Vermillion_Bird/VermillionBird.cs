79b4c1c7406bc905d18e12a0ea7e2c1962e3ea43
Daniel <danieleded2@gmail.com>
2021-02-17 13:02:43 +1300

    Update VermillionBird.cs

--- NPCs/Vermillion_Bird/VermillionBird.cs
+++ NPCs/Vermillion_Bird/VermillionBird.cs
@@ -52,6 +52,7 @@ namespace DynastyMod.NPCs.Vermillion_Bird
 			Sweep
         }
 		private Attack currentAttack = Attack.None;
+		private int Phase = 1;
 
 		// Our AI here makes our NPC sit waiting for a player to enter range, jumps to attack, flutter mid-fall to stay afloat a little longer, then falls to the ground. Note that animation should happen in FindFrame
 		public override void AI()
@@ -65,42 +66,52 @@ namespace DynastyMod.NPCs.Vermillion_Bird
 			if (targetPosition.Y > npc.Center.Y + (10 * CM.Y) && npc.velocity.Y < 4)
 				npc.velocity.Y += DefaultAccelartion.Y; // accelerate down
 
-			npc.velocity.X = (cooldown > 0) ? 0 : (targetPosition.X - npc.Center.X) / (2*CM.X);
+			Action[] Phases = new Action[] { Phase1, Phase2 };
+			Phases[Phase - 1]();
+		}
 
-
-            timer--;
-            cooldown--;
-            if (timer < 0)
-            {
-                currentAttack = Attack.None;
+		private void Phase1()
+		{
+			Vector2 targetPosition = Main.player[npc.target].position;
+			npc.velocity.X = (cooldown > 0) ? 0 : (targetPosition.X - npc.Center.X) / (2 * CM.X);
+			timer--;
+			cooldown--;
+			if (timer < 0)
+			{
+				currentAttack = Attack.None;
 				int RandomTime = WorldGen.genRand.Next(5);
-                switch (WorldGen.genRand.Next(6) % 3)
-                {
-                    case 0: // Tracking Attack
+				switch (WorldGen.genRand.Next(6) % 3)
+				{
+					case 0: // Tracking Attack
 						cooldown = (int)(1.5f * tickSpeed);
 						timer = RandomTime + (1 * tickSpeed) + cooldown;
 						npc.velocity.X = 0;
 						currentAttack = Attack.Tracking;
 						break;
-                    case 1: // Star Attack
+					case 1: // Star Attack
 						cooldown = (int)(1.5f * tickSpeed);
 						timer = RandomTime + (4 * tickSpeed) + cooldown;
 						npc.velocity.X = 0;
-                        currentAttack = Attack.Star;
-                        break;
+						currentAttack = Attack.Star;
+						break;
 					case 2: // Sweep/hellfire Attack
 						cooldown = (int)(1.5f * tickSpeed);
 						timer = RandomTime + (5 * tickSpeed) + cooldown;
 						npc.velocity.X = 0;
 						currentAttack = Attack.Sweep;
-                        SweepDirection = (WorldGen.genRand.Next(2) == 0) ? 1 : -1;
+						SweepDirection = (WorldGen.genRand.Next(2) == 0) ? 1 : -1;
 						break;
-                }
-            }
-            if (currentAttack == Attack.Star && cooldown % (0.25f * tickSpeed) == 0) StarAttack();
+				}
+			}
+			if (currentAttack == Attack.Star && cooldown % (0.25f * tickSpeed) == 0) StarAttack();
 			else if (currentAttack == Attack.Tracking && cooldown % (0.25f * tickSpeed) == 0) TrackingAttack();
 			else if (currentAttack == Attack.Sweep && cooldown % 2 == 0) SweepAttack();
 		}
+
+		private void Phase2()
+        {
+
+        }
 
 		private float SweepDirection = 1;
 		private void SweepAttack()
