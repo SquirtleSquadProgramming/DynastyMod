abb79833dc6c593ebca568c75798369c62935029
NathanKD <111681499+NathanKD@users.noreply.github.com>
2023-07-04 05:44:22 +0000

    Shangren update

--- NPCs/Shangren.cs
+++ NPCs/Shangren.cs
@@ -2,30 +2,45 @@
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.Localization;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria.GameContent.ItemDropRules;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria.GameContent;
 
 namespace DynastyMod.NPCs
 {
     [AutoloadHead]
     public class Shangren : ModNPC
     {
-        private string[] PotenialNPCNames = new string[] { "Fang Li", "Zhang Wei", "Wang Wei", "Li Wei", "Li Hua" };
-
         public override string Texture
         {
             get { return "DynastyMod/NPCs/Shangren"; }
         }
-
+        //Not Used anymore
         /*public override bool Autoload(ref string name)
         {
             name = "Shangren";
             return mod.Properties.Autoload;
         }*/
 
+        public override List<string> SetNPCNameList()
+        {
+            return new List<string>() {
+                "Fang Li",
+                "Zhang Wei",
+                "Wang Wei",
+                "Li Wei",
+                "Li Hua"
+            };
+        }
         public override void SetStaticDefaults()
         {
-            Main.npcFrameCount[NPC.type] = 26;
+            Main.npcFrameCount[NPC.type] = 25;
             NPCID.Sets.ExtraFramesCount[NPC.type] = 9;
-            NPCID.Sets.AttackFrameCount[NPC.type] = 5;
+            NPCID.Sets.AttackFrameCount[NPC.type] = 4;
             NPCID.Sets.DangerDetectRange[NPC.type] = 700;
             NPCID.Sets.AttackType[NPC.type] = 0; // TBE
             NPCID.Sets.AttackTime[NPC.type] = 90; // TBE
@@ -47,6 +62,9 @@ namespace DynastyMod.NPCs
             NPC.DeathSound = SoundID.NPCDeath1;
             NPC.knockBackResist = 0.5f;
             AnimationType = NPCID.Guide;
+            TownNPCStayingHomeless = true;
+
+            //Create Shop here
         }
 
         //public override bool CanTownNPCSpawn(int numTownNPCs, int money)
@@ -63,12 +81,10 @@ namespace DynastyMod.NPCs
         //    }
         //    return false;
         //}
-
-        /*public override string TownNPCName()
+        public override void AI()
         {
-            return PotenialNPCNames[WorldGen.genRand.Next(PotenialNPCNames.Length)];
-        }*/
-
+            NPC.homeless = true;
+        }
         // TBE
         public override string GetChat()
         {
@@ -84,9 +100,8 @@ namespace DynastyMod.NPCs
         public override void OnChatButtonClicked(bool firstButton, ref bool shop)
         {
             if (firstButton)
-            {
                 shop = true;
-            }
+            
         }
 
         public override void SetupShop(Chest shop, ref int slot)
@@ -101,11 +116,8 @@ namespace DynastyMod.NPCs
                 slot++;
             }
         }
-        
-        public override void ModifyNPCLoot(NPCLoot npcloot)
-        { 
-            Item.NewItem(NPC.GetSource_FromThis(),NPC.getRect(), 2);
-        }
+
+        public override void ModifyNPCLoot(NPCLoot npcloot) => npcloot.Add(ItemDropRule.Common(ItemID.GoldCoin, 1));
 
         public override void TownNPCAttackStrength(ref int damage, ref float knockback)
         {
@@ -131,9 +143,28 @@ namespace DynastyMod.NPCs
             randomOffset = 2f;
         }
 
-        public override bool CheckConditions(int left, int right, int top, int bottom)
+        public override bool CheckConditions(int left, int right, int top, int bottom) => base.CheckConditions(left, right, top, bottom);
+        public override ITownNPCProfile TownNPCProfile()
         {
-            return base.CheckConditions(left, right, top, bottom);
+            return new ShangrenProfile();
         }
+    }
+    public class ShangrenProfile : ITownNPCProfile
+    {
+        public int RollVariation() => 0;
+        public string GetNameForVariant(NPC npc) => npc.getNewNPCName();
+
+        public Asset<Texture2D> GetTextureNPCShouldUse(NPC npc)
+        {
+            if (npc.IsABestiaryIconDummy && !npc.ForcePartyHatOn)
+                return ModContent.Request<Texture2D>("DynastyMod/NPCs/Shangren");
+            //From example mod used to put party hat on
+           // if (npc.altTexture == 1)
+               // return ModContent.Request<Texture2D>("ExampleMod/Content/NPCs/ExamplePerson_Party");
+
+            return ModContent.Request<Texture2D>("DynastyMod/NPCs/Shangren");
+        }
+
+        public int GetHeadTextureIndex(NPC npc) => ModContent.GetModHeadSlot("DynastyMod/NPCs/Shangren_Head");
     }
 }
